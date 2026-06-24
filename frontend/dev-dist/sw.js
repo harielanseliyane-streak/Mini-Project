6bae0c6284b18b7a70aabeb5220e866567a86010
harielanseliyane-streak <harielanseliyan.e@gmail.com>
2026-06-24 17:28:33 +0530

    feat: add service worker configuration for offline precaching and routing

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.2ttginf5ur8"
+    "revision": "0.87vqp9k76g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
