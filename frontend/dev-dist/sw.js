a6bb0dcf77ca75f5f51a26396da75c9a5a7df3f9
harielanseliyane-streak <harielanseliyan.e@gmail.com>
2026-06-26 22:38:47 +0530

    feat: implement service worker for precaching and add backend server entry point

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.okea05kqrlg"
+    "revision": "0.14lmjj6n1l"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
