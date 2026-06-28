112f3ff54a04a75bb892026d663d0d45234e172f
harielanseliyane-streak <harielanseliyan.e@gmail.com>
2026-06-28 08:52:04 +0530

    feat: initialize service worker with Workbox precaching and navigation routing

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.tus3h4m4tio"
+    "revision": "0.9cbg1su0uh8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
